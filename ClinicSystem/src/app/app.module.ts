100cc99568932ebca7060ebda1bce23773e4f7da
LordGeese <horusagust@gmail.com>
2021-11-14 12:19:11 +0800

    Added new components
    Added components for client service. Clinics, notifs, messages and appointment components have been added. Will add functionality after navbar is completed

--- ClinicSystem/src/app/app.module.ts
+++ ClinicSystem/src/app/app.module.ts
@@ -6,6 +6,11 @@ import { HeaderComponent } from './main/header/header.component';
 import { MainComponent } from './main/main.component';
 import { BodyComponent } from './main/body/body.component';
 import { FooterComponent } from './main/footer/footer.component';
+import { NavbarComponent } from './pages/navbar/navbar.component';
+import { ClinicsComponent } from './pages/clinics/clinics.component';
+import { AppointmentsComponent } from './pages/appointments/appointments.component';
+import { NotifsComponent } from './pages/notifs/notifs.component';
+import { MessagesComponent } from './pages/messages/messages.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +19,11 @@ import { FooterComponent } from './main/footer/footer.component';
     MainComponent,
     BodyComponent,
     FooterComponent,
+    NavbarComponent,
+    ClinicsComponent,
+    AppointmentsComponent,
+    NotifsComponent,
+    MessagesComponent,
   ],
   imports: [
     BrowserModule
