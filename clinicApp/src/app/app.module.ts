d5c56eb8e3870ed8676c669d86ab772517c007bd
LordGeese <horusagust@gmail.com>
2021-11-15 12:20:03 +0800

    test

--- clinicApp/src/app/app.module.ts
+++ clinicApp/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from "@angular/common";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -36,6 +37,7 @@ import { NavbarComponent } from "./pages/navbar/navbar.component";
   ],
   imports: [
     BrowserModule,
+    CommonModule,
     AppRoutingModule
   ],
   providers: [],
