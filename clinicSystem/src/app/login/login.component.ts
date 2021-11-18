229724280fa111e3817f71ee69238166b772e1a7
LordGeese <horusagust@gmail.com>
2021-11-19 05:56:25 +0800

    MAJOR UPDATE
    login is now functional, with communications to our API
    
    logging in also yields user data, which can be used in other components

--- clinicSystem/src/app/login/login.component.ts
+++ clinicSystem/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from "@angular/router";
 import {LoginService} from "./login.service";
+import {NgForm} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -9,29 +10,18 @@ import {LoginService} from "./login.service";
 })
 export class LoginComponent implements OnInit {
   message: string = "LOGIN";
-  username: string = "";
-  password: string = "";
 
-  constructor(private route: Router, private loginService: LoginService) { }
+  constructor(private route: Router, public loginService: LoginService) { }
 
   ngOnInit(): void {
   }
 
-  checkValues(){
-    if(this.username == "Student"){
-      if(this.password == "Test"){
-        this.message = "LOGIN SUCCESS!";
-        this.loginService.isLoggedIn = true;
-        this.route.navigate(['/clinics']);
-      }
-      else{
-        this.message = "INCORRECT PASSWORD!"
-        setTimeout(()=> this.message = "LOGIN", 2000);
-      }
-    }
-    else{
-      this.message = "INVALID USER!"
-      setTimeout(()=> this.message = "LOGIN", 2000);
+  checkValues(form: NgForm){
+    let email = form.value.email;
+    let password = form.value.password;
+
+    if(form.valid === true){
+      this.loginService.login(email, password);
     }
   }
 }
--- clinicSystem/src/app/login/login.service.ts
+++ clinicSystem/src/app/login/login.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Router} from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
@@ -6,6 +8,29 @@ import { Injectable } from '@angular/core';
 export class LoginService {
 
   isLoggedIn: boolean = false;
+  loginStatus = 'LOGIN';
+  currentUser = '';
+  user_data: any;
 
-  constructor() { }
+  constructor(private httpService: HttpClient, private reroute: Router) { }
+
+  login(email: string, password: string){
+    let form = new FormData();
+    form.append('email', email);
+    form.append('password', password);
+
+    this.httpService.post('http://127.0.0.1:8000/login', form).subscribe((response: any) => {
+      this.currentUser = `${response.first_name} ${response.last_name}`;
+      this.user_data = response.user_data;
+      this.loginStatus = response.message;
+
+      if(this.loginStatus === 'LOGIN SUCCESS'){
+        this.isLoggedIn = true;
+
+        setTimeout( () => {
+          this.reroute.navigate(['/clinics'])
+        }, 1000)
+      }
+    })
+  }
 }
