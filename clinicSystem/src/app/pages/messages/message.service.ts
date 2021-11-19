df2145accc1a057de63f8f2e15c816bdb1991c2e
LordGeese <horusagust@gmail.com>
2021-11-19 10:01:54 +0800

    minor update
    -compose message is now operational
    -API now successfully creates message objects from our validated frontend form

--- clinicSystem/src/app/pages/messages/message.service.ts
+++ clinicSystem/src/app/pages/messages/message.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import {Router} from "@angular/router";
+import {LoginService} from "../../login/login.service";
+import {NgForm} from "@angular/forms";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
 
-  constructor() { }
+  userEmail: string = this.loginService.user_data.email;
+  constructor(private httpService: HttpClient, private reroute: Router, private loginService: LoginService) { }
+
+  sendMessage(form: NgForm){
+    if(form.valid){
+      let formData = new FormData();
+      formData.append('sender', this.userEmail);
+      formData.append('recipient', form.value.recipient);
+      formData.append('content', form.value.messagecontent);
+
+      this.httpService.post('http://127.0.0.1:8000/sendMessage', formData).subscribe((response: any) => {
+        console.log('message sent!');
+        console.log(response.message);
+      })
+    }
+  }
 }
