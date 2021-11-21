6b115452af8846e18b3e2da83bb4c552f07b250c
LordGeese <horusagust@gmail.com>
2021-11-21 15:01:37 +0800

    minor update
    -my appointments component now functional and communicates with APi

--- clinicSystem/src/app/pages/appointments/appointment.service.ts
+++ clinicSystem/src/app/pages/appointments/appointment.service.ts
@@ -2,17 +2,31 @@ import { Injectable } from '@angular/core';
 import {LoginService} from "../../login/login.service";
 import {Router} from "@angular/router";
 import {HttpClient} from "@angular/common/http";
+import {AppointmentModel} from "./appointment.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AppointmentService {
 
+  appointments: AppointmentModel[] = [];
   constructor(private loginService: LoginService, private http: HttpClient, private reroute: Router) { }
 
   getAppointments(){
-    this.http.get(`http://127.0.0.1:8000/getAppointments/${this.loginService}`).subscribe((response: any) => {
-      
+    this.http.get(`http://127.0.0.1:8000/getAppointments/${this.loginService.user_data.id}`).subscribe((response: any) => {
+      this.appointments = [];
+
+      for(let appointment of response.appointments){
+        let newAppointment = new AppointmentModel(
+          appointment.id,
+          appointment.appointment_date,
+          appointment.appointment_status,
+          appointment.category,
+          appointment.doctor,
+          appointment.clinic);
+
+        this.appointments.push(newAppointment);
+      }
     })
   }
 }
--- clinicSystem/src/app/pages/appointments/appointments.component.ts
+++ clinicSystem/src/app/pages/appointments/appointments.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {AppointmentService} from "./appointment.service";
 
 @Component({
   selector: 'app-appointments',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppointmentsComponent implements OnInit {
 
-  constructor() { }
+  constructor(public appointmentService: AppointmentService) { }
 
   ngOnInit(): void {
+    this.appointmentService.getAppointments();
   }
 
 }
