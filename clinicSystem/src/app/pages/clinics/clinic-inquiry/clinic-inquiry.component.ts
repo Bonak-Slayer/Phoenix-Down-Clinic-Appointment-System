b5ddd8dd02b48dd30e5318699499f1b8090decd4
LordGeese <horusagust@gmail.com>
2021-11-22 21:54:47 +0800

    minor update
    -inquiry component is now fully functional
    -template selection is now functional
    -adjusted styling in inquiry component

--- clinicSystem/src/app/pages/clinics/clinic-inquiry/clinic-inquiry.component.ts
+++ clinicSystem/src/app/pages/clinics/clinic-inquiry/clinic-inquiry.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {LoginService} from "../../../login/login.service";
 import {Router} from "@angular/router";
+import {TemplateModel} from "./clinic-inquiry.template-model";
 
 @Component({
   selector: 'app-clinic-inquiry',
@@ -9,11 +10,32 @@ import {Router} from "@angular/router";
 })
 export class ClinicInquiryComponent implements OnInit {
 
+  templateFill: string = '';
+  templates: TemplateModel[] = [];
+
+  firstTemplate = new TemplateModel(1, 'Who are the doctors?');
+  secondTemplate = new TemplateModel(2, 'What machines do you have?');
+  thirdTemplate = new TemplateModel(3, 'Do you offer laboratory tests?');
+
   constructor(private loginService: LoginService, private reroute: Router) { }
 
   ngOnInit(): void {
     if(!this.loginService.isLoggedIn){
       this.reroute.navigate(['/login']);
+    }
+    else{
+      this.templates = [];
+      this.templates.push(this.firstTemplate);
+      this.templates.push(this.secondTemplate);
+      this.templates.push(this.thirdTemplate);
+    }
+  }
+
+  useTemplate(templateId: string){
+    for(let template of this.templates){
+      if (template.id == +templateId) {
+        this.templateFill = template.content;
+      }
     }
   }
 }
---
+++ clinicSystem/src/app/pages/clinics/clinic-inquiry/clinic-inquiry.template-model.ts
@@ -0,0 +1,3 @@
+export class TemplateModel {
+  constructor(public id: number, public content: string){}
+}
